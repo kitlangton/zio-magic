b2e417b1e501a4c2d999ad03d6210a202c14b1f4
Kit Langton <kit.langton@gmail.com>
2021-01-18 20:06:59 -0800

    add a more complex example

---
+++ src/main/scala/zio/magic/ComplexExample.scala
@@ -0,0 +1,240 @@
+package zio.magic
+
+import zio._
+import zio.console.Console
+import zio.magic.ComplexExample.A.A
+import zio.magic.ComplexExample.B.B
+import zio.magic.ComplexExample.C.C
+import zio.magic.ComplexExample.D.D
+import zio.magic.ComplexExample.E.E
+import zio.magic.ComplexExample.F.F
+import zio.magic.ComplexExample.G.G
+import zio.magic.ComplexExample.H.H
+import zio.magic.ComplexExample.I.I
+import zio.magic.ComplexExample.J.J
+import zio.magic.ComplexExample.K.K
+import zio.magic.ComplexExample.L.L
+import zio.magic.ComplexExample.M.M
+
+/**           RIO[A with J, Int]
+  *        A                      J
+  *    B       C                K   L
+  *  D E F   G H I            M
+  */
+private object ComplexExample extends App {
+
+  override def run(args: List[String]): URIO[ZEnv, ExitCode] = {
+
+    val program: ZIO[Console with A with J, Nothing, Unit] =
+      for {
+        a <- ZIO.accessM[A](_.get.string)
+        j <- ZIO.accessM[J](_.get.string)
+        _ <- console.putStrLn(s"Result:\n$a\n$j")
+      } yield ()
+
+    val satisfied: ZIO[Any, E, Unit] =
+      program.provideMagicLayer(
+        Console.live,
+        A.live,
+        J.live,
+        B.live,
+        C.live,
+        D.live,
+        E.live,
+        F.live,
+        G.live,
+        H.live,
+        I.live,
+        K.live,
+        L.live,
+        M.live
+      )
+
+    satisfied.exitCode
+  }
+
+  object A {
+    type A = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[B with C, A] =
+      (for {
+        b <- ZIO.accessM[B](_.get.string)
+        c <- ZIO.accessM[C](_.get.string)
+      } yield new Service {
+        override def string: UIO[String] = UIO(s"A ($b $c)")
+      }).toLayer
+  }
+
+  object B {
+    type B = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[D with E with F, B] =
+      (for {
+        d <- ZIO.accessM[D](_.get.string)
+        e <- ZIO.accessM[E](_.get.string)
+        f <- ZIO.accessM[F](_.get.string)
+      } yield new Service {
+        override def string: UIO[String] = UIO(s"B ($d $e $f)")
+      }).toLayer
+  }
+
+  object C {
+    type C = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[G with H with I, C] =
+      (for {
+        g <- ZIO.accessM[G](_.get.string)
+        h <- ZIO.accessM[H](_.get.string)
+        i <- ZIO.accessM[I](_.get.string)
+      } yield new Service {
+        override def string: UIO[String] = UIO(s"C ($g $h $i)")
+      }).toLayer
+  }
+
+  object D {
+    type D = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[Any, D] = ZLayer.succeed(new Service {
+      override def string: UIO[String] = UIO("D")
+    })
+  }
+
+  object E {
+    type E = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[Any, E] = ZLayer.succeed(new Service {
+      override def string: UIO[String] = UIO("E")
+    })
+  }
+
+  object F {
+    type F = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[Any, F] = ZLayer.succeed(new Service {
+      override def string: UIO[String] = UIO("F")
+    })
+  }
+
+  object G {
+    type G = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[Any, G] = ZLayer.succeed(new Service {
+
+      override def string: UIO[String] = UIO("G")
+
+    })
+  }
+
+  object H {
+    type H = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[Any, H] = ZLayer.succeed(new Service {
+
+      override def string: UIO[String] = UIO("H")
+
+    })
+  }
+
+  object I {
+    type I = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[Any, I] = ZLayer.succeed(new Service {
+      override def string: UIO[String] = UIO("I")
+
+    })
+  }
+
+  object J {
+    type J = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[K with L, J] =
+      (for {
+        k <- ZIO.accessM[K](_.get.string)
+        l <- ZIO.accessM[L](_.get.string)
+      } yield new Service {
+        override def string: UIO[String] = UIO(s"J ($k $l)")
+      }).toLayer
+  }
+
+  object K {
+    type K = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[M, K] =
+      (for {
+        m <- ZIO.accessM[M](_.get.string)
+      } yield new Service {
+        override def string: UIO[String] = UIO(s"K ($m)")
+      }).toLayer
+
+  }
+
+  object L {
+    type L = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[Any, L] = ZLayer.succeed(new Service {
+      override def string: UIO[String] = UIO("L")
+
+    })
+  }
+
+  object M {
+    type M = Has[Service]
+
+    trait Service {
+      def string: UIO[String]
+    }
+
+    def live: URLayer[Any, M] = ZLayer.succeed(new Service {
+      override def string: UIO[String] = UIO("M")
+    })
+  }
+
+}
