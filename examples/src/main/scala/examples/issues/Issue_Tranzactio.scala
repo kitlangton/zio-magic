03a7c3a18037376dad4d2df6988cf4e16ce957fe
Igal Tabachnik <hmemcpy@gmail.com>
2021-03-05 20:18:08 +0200

    Adding an issue demonstrating an inference issue

---
+++ examples/src/main/scala/examples/issues/Issue_Tranzactio.scala
@@ -0,0 +1,55 @@
+package examples.issues
+
+import examples.issues.Transactor.Transactor
+import io.github.gaelrenoux.tranzactio.doobie.Database
+import zio._
+import zio.blocking.Blocking
+import zio.clock.Clock
+import zio.console.putStrLn
+import zio.magic.ZioProvideMagicOps
+
+import javax.sql.DataSource
+
+object Transactor {
+  type Transactor = Has[Database.Service]
+
+  final case class DbConfig(username: String)
+
+  private val dsLayer: ZLayer[Blocking, Nothing, Has[DataSource]] = ZLayer.succeed(null)
+
+  val live: ZLayer[Blocking with Clock, Nothing, Transactor] =
+    (dsLayer ++ Blocking.any ++ Clock.any) >>> Database.fromDatasource
+}
+
+class Issue_Tranzactio {
+  object MyService {
+    type MyService = Has[Service]
+    trait Service {
+      def plusOne(i: Int): ZIO[Transactor, Nothing, Int]
+    }
+
+    val live: ZLayer[Any, Nothing, Has[Service]] =
+      ZLayer.succeed((_: Int) => UIO(1))
+
+    def plusOne(i: Int): RIO[MyService with Transactor, Int] = ZIO.accessM(_.get.plusOne(i))
+  }
+
+  object Main extends zio.App {
+
+    override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
+      val services = List(
+        MyService.plusOne(1),
+//        MyService.plusOne(1)  // uncomment this line and the problem will occur
+      )
+      val program = ZIO.collectAll_(services)
+
+      program
+        .provideCustomMagicLayer(
+          MyService.live,
+          Transactor.live
+        )
+        .exitCode
+    }
+  }
+
+}
